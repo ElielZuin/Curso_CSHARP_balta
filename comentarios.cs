6649df422be3531aa849a67d8488012e92455684
ElielZuin <profissional.elielzuin@gmail.com>
2025-01-02 15:06:40 -0300

    comentarios

---
+++ comentarios.cs
@@ -0,0 +1,40 @@
+//importações
+using System;
+using System.ComponentModel.Design.Serialization;
+using System.Diagnostics;
+using Microsoft.VisualBasic;
+
+
+// namespace
+namespace Teste
+{
+    // classe principal do programa
+    class Program1
+    {
+        //função principal do programa
+        static void Main1(string[] args)
+        {
+            var texto = "Testando aa";
+                                                    // control+k+c pra comentar os selecionados e ctrl+k+u pra descomentar! ou so //
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(texto);
+        }
+    }
+}                                                               //aqui em baixo tem mais!
+
+            /* comentariooo
+            mais uma linha
+            e etc, pra fazer assim é
+            so usar o /*mensagem */
+
+
+
+                            //metadados:  usar 3 barras
+                            /// escrever aqui as info das classes!
+
+
+
+
+
+
+

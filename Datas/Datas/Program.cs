cf357f62b9006046b29f45c0b12fb0e62ef4cf17
ElielZuin <142356445+ElielZuin@users.noreply.github.com>
2025-04-30 17:53:47 -0300

    Add files via upload

--- Datas/Datas/Program.cs
+++ Datas/Datas/Program.cs
@@ -13,9 +13,15 @@ namespace Datas
             //var data = new DateTime();
 
             //var data = DateTime.Now;
+            FormatandoDatas();
 
-            var data = new DateTime(2025, 04, 24, 23,53,38);
-            
+
+        }
+
+        private static void CriandoDataNova()
+        {
+            var data = new DateTime(2025, 04, 25, 00, 57, 00);
+
             Console.WriteLine(data);
             Console.WriteLine(data.Year);
             Console.WriteLine(data.Month);
@@ -25,6 +31,51 @@ namespace Datas
             Console.WriteLine(data.Second);
             Console.WriteLine(data.DayOfWeek);
             Console.WriteLine(data.DayOfYear);
+
+            Console.WriteLine((int)data.DayOfWeek);
+        }
+
+        static void FormatandoDatas()
+        {
+            Console.Clear();
+            var data = DateTime.Now;
+
+            // y -> Ano
+            // M -> Mes
+            // d -> Dia
+            // h -> Hora
+            // m -> Minuto
+            // s -> Segundo
+            // ff -> fracao de segundo
+            // zzz -> timezone (-3uth)
+
+            var formatada = String.Format("{0:yyyy}", data); //assim so exibe o ano
+                                                              // se for 0:y ele exibe
+                                                              // o mes por extenso e
+                                                              // o ano
+            // mas se for 0:M ele exibe o mes e o dia
+            Console.WriteLine(formatada);
+
+            var formatada1 = String.Format("{0:dd/MM/yyyy }", data);
+            
+            Console.WriteLine(formatada1);
+
+            var formatardata = String.Format("{0:dd/MM/yyyy hh:mm:ss ff z}", data);
+            Console.WriteLine(formatardata);
+
+        }
+
+        static void PadroesDeFormatacao()
+        {
+            var data = DateTime.Now;
+            var formatada = String.Format("{0:d}", data); //exibe a menor data possivel
+            //da mesma forma; T maiusculo exibe o tempo longo ex 1:54:32 PM
+            // D maiusculo exibe por extenso o nome do dia e tal
+            // f combina data e hora
+            // g combina data e hora so que no short( curto)
+            
+            
+            Console.WriteLine(formatada);
         }
     }
 }
